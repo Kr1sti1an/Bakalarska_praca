c5f1a63f40387feb49d78b2c27fc9f3b95f5d531
Kr1sti1an <slatkovskykristian@gmail.com>
2024-12-22 12:25:51 +0100

    commit 2 - Arduino_ovladac
    pridanie časti kódu pre čítanie hodnoty z potenciometra, ktorý bude slúžiť ako volant na našom ovládači. Zatial sa len hodnoty vypisujú na konzolu. Pridaný aj delay pre stabilnejšiu komunikáciu.

--- Arduino/Arduino_ovladac_v.1/Arduino_ovladac_v.1.ino
+++ Arduino/Arduino_ovladac_v.1/Arduino_ovladac_v.1.ino
@@ -5,11 +5,16 @@ void setup() {
 }
 
 void loop() {
+  int potValue = analogRead(A0); // Čítanie potenciometra (0 - 1023)
   bool forwardButton = !digitalRead(3); // Čítanie stavu ľavého tlačidla
   bool brakeButton = !digitalRead(2);   // Čítanie stavu pravého tlačidla
 
   // Posielame údaje vo formáte: "forwardButton, brakeButton", údaje sa vypisujú na konzole
+  Serial.print(potValue);
+  Serial.print(",");
   Serial.print(forwardButton);
   Serial.print(",");
   Serial.println(brakeButton);
+
+  delay(50); // Malé oneskorenie pre stabilnejšiu komunikáciu
 }
